46738fd9869c29c7e3d72601d960d380b0868c17
Rafael Holanda <rafaelholanda90@gmail.com>
2022-09-07 16:09:17 -0300

    Change to reverse and not printing result on aloc func

--- samples/aloc.go
+++ samples/aloc.go
@@ -1,18 +1,14 @@
 package samples
 
-import "fmt"
-
 func Aloc[O any, T []O](obj O, times int) (result T) {
 
 	for i := 0; i <= times; i++ {
 		result = append(result, obj)
 	}
 
-	values := make([]any, 0)
-	for _, o := range result {
-		values = append(values, fmt.Sprintf("%+v\n", o))
+	for i, o := range result {
+		result[len(result)-(i+1)]=o
 	}
-	fmt.Println(values...)
 
 	return
 }
