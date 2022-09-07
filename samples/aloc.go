8dd7d4618fe604a03ef7b31a471967c02b61c18e
Rafael Holanda <rafaelholanda90@gmail.com>
2022-09-07 16:47:20 -0300

    Validate an error avoiding returning any result with err

--- samples/aloc.go
+++ samples/aloc.go
@@ -1,18 +1,22 @@
 package samples
 
-import "fmt"
-
 func Aloc[O any, T []O](obj O, times int) (result T) {
 
 	for i := 0; i <= times; i++ {
 		result = append(result, obj)
 	}
 
-	values := make([]any, 0)
 	for _, o := range result {
-		values = append(values, fmt.Sprintf("%+v\n", o))
+		if isError(o) {
+			result = nil
+			return
+		}
 	}
-	fmt.Println(values...)
 
 	return
+}
+
+func isError(o any) bool {
+	_, ok := o.(error)
+	return ok
 }
--- samples/aloc_test.go
+++ samples/aloc_test.go
@@ -2,6 +2,7 @@ package samples_test
 
 import (
 	"testing"
+	"errors"
 
 	"github.com/rafaelhl/profiling-samples/samples"
 	"github.com/stretchr/testify/assert"
@@ -11,4 +12,10 @@ func TestAloc(t *testing.T) {
 	result := samples.Aloc("test", 10)
 
 	assert.Len(t, result, 11)
+}
+
+func TestAlocError(t *testing.T) {
+	result := samples.Aloc(errors.New("test"), 10)
+
+	assert.Len(t, result, 0)
 }
